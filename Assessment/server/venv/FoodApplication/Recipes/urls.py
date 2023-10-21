70188af3433a76bdf34066323a75ee9b5d18e58d
Pavani-Dulam283 <134054341+Pavani-Dulam283@users.noreply.github.com>
2023-10-21 17:37:40 +0530

    Delete Assessment directory

--- Assessment/server/venv/FoodApplication/Recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('res/',RecipeAPIView.as_view()),
-    path('res/<int:id>/',RecipeAPIView.as_view()),
-    path('edit/<int:id>/',EditAPIView.as_view()),
-    path('week/<str:week>/',dateAPIView.as_view()),
-]
