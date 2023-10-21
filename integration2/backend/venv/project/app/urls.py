4d02c761bbf12834ef6be176fb5ef43c42ab1304
Pavani-Dulam283 <134054341+Pavani-Dulam283@users.noreply.github.com>
2023-10-21 17:39:17 +0530

    Delete integration2 directory

--- integration2/backend/venv/project/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns=[
-    path('person/',PersonAPIView.as_view()),
-    path('person/<int:id>/',PersonAPIView.as_view()),
-    path('per/<int:id>/',Person2APIView.as_view()),
-    path('search/',SearchAPIView.as_view())
-]
--- integration2/frontend/persons/src/components/Delete.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Delete=()=>{
-    return(
-        <h1>hii</h1>
-    )
-
-}
-export default Delete;
